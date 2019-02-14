9b097db4586de7140b3924da0b2aa104d4ecae54
julio.trindade <vsm.julio@gmail.com>
2019-02-14 08:24:30 -0200

    Initial commit

---
+++ src/main/java/com/vsm/devcase/DevcaseApplication.java
@@ -0,0 +1,14 @@
+package com.vsm.devcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevcaseApplication.class, args);
+    }
+
+}
+
